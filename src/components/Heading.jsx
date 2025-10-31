9f8373f645ebc5420d7ea88b0fb506166debdf50
Shadrack <tuyizeresh@gmail.com>
2025-10-31 11:55:51 +0200

    Add responsive design

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 function Heading (){
     return (
         <div>
-<h1 className="text-[45px] font-bold pt-20 pb-10">We partner with the world's <br />leading universities</h1>
+<h1 className="text-[45px] sm:text-[25px] md:text-[30px] lg:text-[45px] font-bold pt-20 pb-10">We partner with the world's <br />leading universities</h1>
         </div>
     )
 }
